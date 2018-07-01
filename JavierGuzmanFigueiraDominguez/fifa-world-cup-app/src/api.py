23371af973776178794249cb8591c159c574b91f
jgoodman8 <centoloviolino@gmail.com>
2018-07-01 09:55:48 +0200

    Added UI to the queries previously defined - Using bottle framework

---
+++ JavierGuzmanFigueiraDominguez/fifa-world-cup-app/src/api.py
@@ -0,0 +1,92 @@
+import random
+from bottle import Bottle, run
+from repository import Repository
+
+repository = Repository()
+repository.load_graph()
+app = Bottle()
+tables_class = "mdl-data-table mdl-js-data-table mdl-shadow--2dp"
+
+
+@app.route('/')
+def render_tables():
+    data = repository.get_info()
+    data.columns = ['Place', 'Total Attendance', 'Winner', 'Year']
+
+    return add_html(data.to_html(classes=tables_class, index=False, col_space=300))
+
+
+@app.route('/years/<year>')
+def render_tables(year):
+    data = repository.get_info_by_year(year)
+    data.columns = ['Attendance', 'City', 'MatchID', 'DateTime']
+
+    return add_html(data.to_html(classes=tables_class, index=False))
+
+
+@app.route('/teams')
+def render_tables():
+    data = repository.get_team_ids()
+    data.columns = ['Team Code', 'Team']
+
+    return add_html(data.to_html(classes=tables_class, index=False))
+
+
+@app.route('/goals')
+def render_tables():
+    data = repository.get_goals_by_team()
+    data.columns = ['Team Code', 'Goals Scored']
+
+    return add_html(data.to_html(classes=tables_class, index=False))
+
+
+@app.route('/players')
+def render_tables():
+    data = repository.get_matches_by_player()
+    data.columns = ['Games played', 'Player']
+
+    return add_html(data.to_html(classes=tables_class, index=False))
+
+
+def add_html(table):
+    years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994,
+             1998, 2002, 2006, 2010, 2014]
+    year = random.SystemRandom().choice(years)
+
+    return """
+        <html>
+        <head>
+            <title>FIFA World Cup</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+            <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+            <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        </head>
+        <body style="padding-top:14px;background-color:"#eee">
+            <div style="padding-left:30px;padding-right:24px;padding-bottom:20px;">
+                <div>
+                     <a href="/" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Years
+                    </a>
+                    <a href="/teams" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Teams
+                    </a>
+                    <a href="/goals" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Goals
+                    </a>
+                    <a href="/players" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Players
+                    </a>
+                    <a href="/years/""" + str(year) + """"
+                     class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Random Edition
+                    </a>
+                </div>
+                <br/>
+    """ + table + """
+        </div>
+        </body>
+        </html>
+    """
+
+
+run(app, host='localhost', port=8080)
---
+++ JavierGuzmanFigueiraDominguez/fifa-world-cup-app/src/repository.py
@@ -0,0 +1,118 @@
+from rdflib import Graph
+import pandas as pd
+
+
+class Repository:
+
+    def __init__(self):
+        pd.set_option('display.max_colwidth', -1)
+        self.graph = None
+        self.prefix = """
+            PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+            PREFIX schema: <http://schema.org/>
+            PREFIX fwc: <http://linkeddata.fifawordcup.org/ontology/WordCup#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX gn: <http://www.geonames.org/ontology#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX event: <http://purl.org/NET/c4dm/event.owl#>
+            PREFIX sport: <http://www.bbc.co.uk/ontologies/sport/>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        """
+
+    def load_graph(self):
+        print('Loading data...')
+
+        self.graph = Graph()
+        self.graph.parse('../../data/target/word_cup.ttl', format='turtle')
+
+        print('Data loaded')
+
+    def get_info(self):
+        query = """        
+            SELECT ?year ?winner ?place ?totalAttendance
+            WHERE { 
+                ?year fwc:isWinner ?winner .
+                ?year event:place ?place .
+                ?year fwc:totalAttendance ?totalAttendance .
+            }
+            ORDER BY ASC(?year)
+        """
+        result = self.graph.query(self.prefix + query)
+
+        return pd.DataFrame(result.bindings)
+
+    def get_info_by_year(self, year):
+        query = """
+            SELECT ?match ?time ?city ?attendance
+            WHERE { 
+                ?year sport:hasMatch ?match .
+                ?match event:time ?time .
+                ?match event:place ?stadium .
+                ?stadium foaf:based_near ?city .
+                ?match fwc:totalAttendance ?attendance .
+                FILTER (?year = <http://linkeddata.fifawordcup.org/resources/Year/""" + str(year) + """>)
+            } 
+            ORDER BY ASC(?time)
+        """
+        result = self.graph.query(self.prefix + query)
+
+        return pd.DataFrame(result.bindings)
+
+    def get_team_ids(self):
+        query = """
+            SELECT DISTINCT ?team ?code
+            WHERE {
+                { 
+                    ?match sport:homeCompetitor ?team . 
+                    ?team gn:countryCode ?code .
+                } 
+                    UNION
+                { 
+                    ?match sport:awayCompetitor ?team . 
+                    ?team gn:countryCode ?code .
+                }
+            }
+            ORDER BY ASC(?team)
+        """
+        result = self.graph.query(self.prefix + query)
+
+        return pd.DataFrame(result.bindings)
+
+    def get_goals_by_team(self):
+        query = """
+            SELECT ?code (SUM(?goals) AS ?sum)
+            WHERE {
+                {
+                    ?match sport:homeCompetitor ?team .
+                    ?team gn:countryCode ?code .
+                    ?match fwc:homeGoals ?goals .
+                }
+                    UNION
+                {
+                    ?match sport:awayCompetitor ?team .
+                    ?team gn:countryCode ?code .
+                    ?match fwc:awayGoals ?goals .
+                }
+            }
+            GROUP BY ?team
+            ORDER BY DESC(?sum)
+        """
+        result = self.graph.query(self.prefix + query)
+
+        return pd.DataFrame(result.bindings)
+
+    def get_matches_by_player(self):
+        query = """
+            SELECT ?playerName (COUNT(?playerName) AS ?count)
+            WHERE {
+                ?player fwc:fullName ?playerName
+            }
+            GROUP BY ?playerName
+            ORDER BY DESC(?count)
+            LIMIT 50
+        """
+        result = self.graph.query(self.prefix + query)
+
+        return pd.DataFrame(result.bindings)
